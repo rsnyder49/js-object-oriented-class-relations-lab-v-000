cd805f7d91b3e7e822bc99a9dfb1790d72d144f6
Robert Snyder <r.snyder49@gmail.com>
2018-12-26 20:26:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 let store = { drivers: [], trips: [], passengers: [] };
 
-let driverId = 0;
+let userId = 0;
 
 class Driver {
   constructor(name) {
     this.name = name;
-    this.id = ++driverId;
+    this.id = ++userId;
   }
 }
